62bcf0187c50d8bf36577e7f43b4493b40505e3b
Cristian Sanchez <cdss2911@gmail.com>
2026-03-02 21:47:08 -0500

    ambiente productivo

---
+++ passenger_wsgi.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+print("___________________________________________________________")
+print("Incializando...")
+
+BASE_DIR = os.path.dirname(__file__)
+if BASE_DIR not in sys.path:
+    sys.path.insert(0, BASE_DIR)
+
+from app import create_app
+
+application = create_app()
